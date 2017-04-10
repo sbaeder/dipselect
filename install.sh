e6e8ee68a2494f61e427933396b1817945252478
zwack-am <zwack.am@gmail.com>
2017-04-10 13:22:13 -0700

    Added enablement of service to install script

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 cp dipselect.service /etc/systemd/system/dipselect.service
 cp dipselect.sh /usr/local/bin/dipselect.sh
 cp xorg.conf.* /etc/X11
-ln -s /etc/systemd/system/dipselect.service /etc/systemd/system/multi-user.wants
+systemctl enable dipselect
 /usr/local/bin/dipselect.sh
