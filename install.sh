3c19af95475de37f461654526fe17e7186d81707
zwack-am <zwack.am@gmail.com>
2017-04-10 13:31:03 -0700

    Saved original xorg.conf

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 cp dipselect.service /etc/systemd/system/dipselect.service
 cp dipselect.sh /usr/local/bin/dipselect.sh
+cp /etc/X11/xorg.conf /etc/X11/xorg.conf.orig
 cp xorg.conf.* /etc/X11
 systemctl enable dipselect
 /usr/local/bin/dipselect.sh
