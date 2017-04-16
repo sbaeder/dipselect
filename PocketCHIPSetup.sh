15d02b5baeb263d050e57bf0510fc296f0aa4ae3
zwack-am <zwack.am@gmail.com>
2017-04-15 17:37:58 -0700

    Added the PocketCHIP dependencies

--- PocketCHIPSetup.sh
+++ PocketCHIPSetup.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 
+echo deb http://opensource.nextthing.co/chip/debian/pocketchip jessie main >> /etc/apt/sources.list
 apt-get update
-apt-get -y install LIST OF PACKAGES
+apt-get -y install ahoy awesome chip-sunvox pcmanfm pocketchip-batt pocketchip-configs pocketchip-list pocketchip-load pocketchip-localdoc pocketchip-onboard pocketchip-update pocket-home pocket-wm surf vala-terminal
