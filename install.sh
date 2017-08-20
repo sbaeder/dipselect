282041ae48f2e59817a65f109a7a4851fb188a63
D Scott Baeder <sbaeder@aol.com>
2017-08-20 15:07:38 -0400

    Update install.sh
    Add code to make sure we can read the eeproms

--- install.sh
+++ install.sh
@@ -6,6 +6,23 @@ echo with the PocketCHIP-setup.sh script provided.
 echo Configurations have been provided for PocketCHIP, HDMI and
 echo TZATZIFFY DIPs.  A template has also been provided for
 echo other DIPs. These are kept in /usr/local/share/dipselect.
+echo
+echo
+if [ "$EUID" -ne 0 ]
+  then echo "This needs to be run as root :-( "
+  exit
+fi
+#
+if grep -Fxq "w1_ds2431" /etc/modules
+then
+	echo "w1_ds2431 already in autoload..."
+else
+	echo "w1_ds2431" >> /etc/module
+  echo "Module ws_d2431 has been added to read eeprom added..."
+	echo "Please REBOOT NOW and re-run install.sh"
+	exit
+fi
+
 cp dipselect.service /etc/systemd/system/dipselect.service
 cp dipselect.sh /usr/local/bin/dipselect.sh
 cp /etc/X11/xorg.conf /etc/X11/xorg.conf.orig
